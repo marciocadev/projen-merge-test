bf281336cc4a5199bb0926d43e59702c6ee1d9f3
Marcio Almeida <marcioadev@gmail.com>
2022-06-22 11:33:24 -0300

    feat(output): output without export

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    new CfnOutput(this, 'Output', {
+      value: 'output',
+    });
   }
 }
 
